0be5603764f55d2d45f08a73ccbb3047a2167f96
endure. <enduredev@outlook.com>
2024-11-04 19:44:44 +0000

    Update calendar.tsx

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -58,8 +58,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeftIcon className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRightIcon className="h-4 w-4" />,
+        IconLeft: (iconProps) => <ChevronLeftIcon className="h-4 w-4" {...iconProps} />,
+        IconRight: (iconProps) => <ChevronRightIcon className="h-4 w-4" {...iconProps} />,
       }}
       {...props}
     />
